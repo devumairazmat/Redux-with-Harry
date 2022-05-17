0af55541abc7de022ca58efaf32a0bef7f1a7b52
Umair Azmat <umairazmat8260@gmail.com>
2022-05-18 01:14:25 +0500

    Withdrawl / deposit Designed

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Shop() {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <div className="container text-center">
+        <div className="row mt-4 mb-4">
+          <div className="col"></div>
+          <div className="col">
+            <h4 className="text-center mt-4 mb-4">
+              Buy Iphone 13 for 3 lac Rs/Pkr
+            </h4>
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic mixed styles example"
+            >
+              <button type="button" class="btn btn-danger">
+                -
+              </button>
+              <button disabled type="button" class="btn btn-warning">
+                <h5></h5> Deposit / Withdrawl</h5>
+              </button>
+              <button type="button" class="btn btn-success">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="col"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
