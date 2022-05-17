87e131f6a099da82d2b2eb7def89dbdfee739058
Umair Azmat <umairazmat8260@gmail.com>
2022-05-18 02:12:20 +0500

    Store Created

---
+++ src/state/Store.js
@@ -0,0 +1,6 @@
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
+import { createStore } from "redux";
+
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
